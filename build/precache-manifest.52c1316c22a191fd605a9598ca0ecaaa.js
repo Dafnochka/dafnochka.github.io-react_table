f5c101cbd6d724adb97bc394345e2f8fe198cc6d
Dafnochka <dlushpina@gmail.com>
2019-01-30 15:27:02 +0300

    Удалила build из игнора

---
+++ build/precache-manifest.52c1316c22a191fd605a9598ca0ecaaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "208f08cd2c0920dee506",
+    "url": "/table/static/js/runtime~main.208f08cd.js"
+  },
+  {
+    "revision": "061e4d2f29c4c84f763b",
+    "url": "/table/static/js/main.061e4d2f.chunk.js"
+  },
+  {
+    "revision": "53f92add37229ee2dc39",
+    "url": "/table/static/js/1.53f92add.chunk.js"
+  },
+  {
+    "revision": "061e4d2f29c4c84f763b",
+    "url": "/table/static/css/main.16e6d36f.chunk.css"
+  },
+  {
+    "revision": "d771416c3b28abd63918d34f9a6e4748",
+    "url": "/table/index.html"
+  }
+];
