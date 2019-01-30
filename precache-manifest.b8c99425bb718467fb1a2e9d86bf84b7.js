673be7032969d21e6be9c2661949cfec798b8c74
Dafnochka <dlushpina@gmail.com>
2019-01-30 15:27:02 +0300

    размещение сборки

---
+++ precache-manifest.b8c99425bb718467fb1a2e9d86bf84b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "061e4d2f29c4c84f763b",
+    "url": "/static/js/main.061e4d2f.chunk.js"
+  },
+  {
+    "revision": "53f92add37229ee2dc39",
+    "url": "/static/js/1.53f92add.chunk.js"
+  },
+  {
+    "revision": "061e4d2f29c4c84f763b",
+    "url": "/static/css/main.16e6d36f.chunk.css"
+  },
+  {
+    "revision": "f9e0a0f2c2b3966dac287765ab4d1037",
+    "url": "/index.html"
+  }
+];
